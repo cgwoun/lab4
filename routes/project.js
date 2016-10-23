44bc41ced84e65c3f5e2dba917fd8e129a44df55
Chang <cgwoun21@gmail.com>
2016-10-22 20:53:30 -0700

    Lab 4 complete

---
+++ routes/project.js
@@ -0,0 +1,8 @@
+exports.viewProject = function(req, res){
+	//controller goes here
+	var name = req.params.name;
+	console.log('The project name is: ' + name);
+	res.render('project', {
+		'projectName': name
+	});
+}
